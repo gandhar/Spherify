988ae7cd23cd4248cd60cb3782b82f23c7e845ea
gandhar <gandhar0@gmail.com>
2014-01-28 09:35:21 +0530

    better

--- src/gandhar/spherify/EditActivity.java
+++ src/gandhar/spherify/EditActivity.java
@@ -27,7 +27,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
